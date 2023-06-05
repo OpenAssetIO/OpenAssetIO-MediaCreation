e41050c80e48f57a1abde63d6805b13187479965
Tom Cowland <tom@foundry.com>
2023-06-05 14:56:42 +0100

    [Test] Simplify C++ header imports
    We previously explicitly imported all the C++ headers. This served to
    verify that the YAML produced the correct outputs. We also do this in
    Python though, so this is really mostly testing TraitGen.
    
    We have a rule of thumb that we don't re-test auto-generated code, and
    the C++ tooling requirements have been noted to form a barrier to entry
    to contributors who may work mostly in the Python world.
    
    As, assuming we trust TraitGen, all generated headers are still imported
    by the package header, this commit removes the explicit imports of
    individual trait headers. This means we should still test that all
    shipping headers are syntax correct.
    
    Signed-off-by: Tom Cowland <tom@foundry.com>

--- tests/cpp/test.cpp
+++ tests/cpp/test.cpp
@@ -1,17 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2022 The Foundry Visionmongers Ltd
 
-// Include all headers to test they are where we expect and can be
-// compiled.
+// Include all headers to test they can be compiled.
 
 #include <openassetio_mediacreation/openassetio_mediacreation.hpp>
-#include <openassetio_mediacreation/traits/content.hpp>
-#include <openassetio_mediacreation/traits/content/LocatableContentTrait.hpp>
-#include <openassetio_mediacreation/traits/identity/DisplayNameTrait.hpp>
-#include <openassetio_mediacreation/traits/managementPolicy.hpp>
-#include <openassetio_mediacreation/traits/managementPolicy/ManagedTrait.hpp>
-#include <openassetio_mediacreation/traits/managementPolicy/ResolvesFutureEntitiesTrait.hpp>
-#include <openassetio_mediacreation/traits/traits.hpp>
 
 using namespace openassetio_mediacreation;
 
