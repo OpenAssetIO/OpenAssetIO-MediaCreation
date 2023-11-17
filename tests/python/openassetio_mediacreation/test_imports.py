2bef57eb856091832e0c622ce2de635ccbe57f83
Tom Cowland <tom@foundry.com>
2023-11-17 16:05:40 +0000

    [Tests] Restructure Python test cases
    Groups tests by namespace, to simplify management when we add new
    traits.
    
    Part of #22
    
    Signed-off-by: Tom Cowland <tom@foundry.com>

--- tests/python/openassetio_mediacreation/test_imports.py
+++ tests/python/openassetio_mediacreation/test_imports.py
@@ -22,24 +22,44 @@ def always_unload_openassetio_mediacreation_modules(
     """
 
 
-class Test_trait_imports:
+class Test_package_imports:
     def test_importing_openassetio_mediacreation_succeeds(self):
         import openassetio_mediacreation
 
     def test_importing_traits_succeeds(self):
         from openassetio_mediacreation import traits
 
-    def test_importing_content_succeeds(self):
+    def test_importing_specifications_succeeds(self):
+        from openassetio_mediacreation import specifications
+
+
+class Test_trait_imports_content:
+    def test_importing_namespace_succeeds(self):
         from openassetio_mediacreation.traits import content
 
     def test_importing_LocatableContentTrait_succeeds(self):
         from openassetio_mediacreation.traits.content import LocatableContentTrait
 
+
+class Test_trait_imports_managementPolicy:
+    def test_importing_namespace_succeeds(self):
+        from openassetio_mediacreation.traits import managementPolicy
+
     def test_importing_ManagedTrait_succeeds(self):
         from openassetio_mediacreation.traits.managementPolicy import ManagedTrait
 
+
+class Test_trait_imports_identity:
+    def test_importing_namespace_succeeds(self):
+        from openassetio_mediacreation.traits import identity
+
     def test_importing_DisplayNameTrait_succeeds(self):
         from openassetio_mediacreation.traits.identity import DisplayNameTrait
+
+
+class Test_trait_imports_auth:
+    def test_importing_namespace_succeeds(self):
+        from openassetio_mediacreation.traits import auth
 
     def test_importing_BearerToken_succeeds(self):
         from openassetio_mediacreation.traits.auth import BearerTokenTrait
